6782ce4a04792ac609de4942965cdaae0e03f763
Everaldo Canuto <everaldo.canuto@gmail.com>
2024-02-26 03:47:35 -0300

    Update test workflow

--- builder.sh
+++ builder.sh
@@ -5,6 +5,10 @@ DEBIAN_VERSION="12"
 DEBIAN_VERNAME="bookworm"
 DEBIAN_RELEASE="20240201-1644"
 
+function function_exists() {
+	declare -F "$1" > /dev/null;
+}
+
 function do_relase() {
 	echo gh release create "${GITHUB_REF_NAME}" \
 		--repo="${GITHUB_REPOSITORY}" \
@@ -23,6 +27,6 @@ echo "DEBIAN_VERSION=$DEBIAN_VERSION" >> $GITHUB_ENV
 echo "DEBIAN_VERNAME=$DEBIAN_VERNAME" >> $GITHUB_ENV
 echo "DEBIAN_RELEASE=$DEBIAN_RELEASE" >> $GITHUB_ENV
 
-if [[ "$1" == "relase" ]]; then
+if "$1" == "release"; then
 	do_relase
 fi
