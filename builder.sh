400c4750ad0efa2549091a0df09002531f1ed4e1
Everaldo Canuto <everaldo.canuto@gmail.com>
2024-02-26 05:59:26 -0300

    Update test workflow

--- builder.sh
+++ builder.sh
@@ -4,33 +4,16 @@
 DEBIAN_VERSION="12"
 DEBIAN_VERNAME="bookworm"
 DEBIAN_RELEASE="20240201-1644"
-
-function function_exists() {
-	declare -F "$1" > /dev/null;
-}
-
-function do_relase() {
-	# echo gh release create "${GITHUB_REF_NAME}" \
-	# 	--repo="${GITHUB_REPOSITORY}" \
-	# 	--title="${GITHUB_REF_NAME}" \
-	# 	--generate-notes \
-	# 	--latest \
-	# 	--draft
-
-	echo gh release create --verify-tag
-}
-
-echo "---1"
-echo "$@"
-echo "---2"
+DEBIAN_VARIANT="genericcloud-amd64"
 
 # Export variables to github workflow next steps
 cat >> "$GITHUB_ENV" <<-EOF
 	DEBIAN_VERSION="$DEBIAN_VERSION"
 	DEBIAN_VERNAME="$DEBIAN_VERNAME"
 	DEBIAN_RELEASE="$DEBIAN_RELEASE"
+	DEBIAN_RELEASE="$DEBIAN_VARIANT"
 EOF
 
-if [ "$1" == "release" ]; then
-	do_relase
-fi
+echo "---1"
+echo "$@"
+echo "---2"
