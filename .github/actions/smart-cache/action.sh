7615040c07af3d9dfb185c21a71faebddb8ff38a
Everaldo Canuto <everaldo.canuto@gmail.com>
2024-02-26 15:49:31 -0300

    Update test workflow

--- .github/actions/smart-cache/action.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-gh release create "${GITHUB_REF_NAME}" \
-    --repo="${GITHUB_REPOSITORY}" \
-    --title="${GITHUB_REF_NAME}" \
-    --verify-tag \
-    --latest \
-    --draft
