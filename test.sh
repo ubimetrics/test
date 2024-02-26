93c48f8386e4571cb9a16abd2837cafd7353b935
Everaldo Canuto <everaldo.canuto@gmail.com>
2024-02-26 03:08:46 -0300

    Update test workflow

--- test.sh
+++ test.sh
@@ -6,3 +6,7 @@ echo $@
 echo ---
 
 echo $GITHUB_ENV
+
+echo 'DEBIAN_VERSION="12"' >> $GITHUB_ENV
+echo 'DEBIAN_VERNAME="bookworm"' >> $GITHUB_ENV
+echo 'DEBIAN_RELEASE="20240201-1644"' >> $GITHUB_ENV
