d36038814613d63b8d86205bdc09f6fcf2dbc1f2
Everaldo Canuto <everaldo.canuto@gmail.com>
2024-03-09 14:20:38 -0300

    Update release.yml

--- settings.sh
+++ settings.sh
@@ -25,17 +25,16 @@ fi
 # Image packages
 declare -A PACKAGES
 PACKAGES=(
+    [xqemu]=qemu-guest-agent
     [locales]=qemu-guest-agent,locales-all
     [extras]=qemu-guest-agent,locales-all,htop
-    [qemu]=qemu-guest-agent
 )
 
-IMAGE_PACKAGES="${PACKAGES[$1]}"
 if [[ "$1" != "" && -v "PACKAGES[$1]" ]]; then
-	# IMAGE_PACKAGES="${PACKAGES[$1]}"
+	IMAGE_PACKAGES="${PACKAGES[$1]}"
 	echo ">>> true <<<" "$1" "${PACKAGES[$1]}"
 else
-	# IMAGE_PACKAGES="qemu-guest-agent"
+	IMAGE_PACKAGES="qemu-guest-agent"
 	echo ">>> false <<<"
 fi
 
