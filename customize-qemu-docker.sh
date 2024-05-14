825e61a1f06de8e1152fc584bb4f24f1cb08aabe
Everaldo Canuto <everaldo.canuto@gmail.com>
2024-05-14 17:32:51 -0300

    Update release.yml

--- customize-qemu-docker.sh
+++ customize-qemu-docker.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
-asddas
+# Add Docker's official GPG key
+curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 
-apt-get --yes install qemu-guest-agent
+# Add Docker's official repository to Apt sources
+cat > /etc/apt/sources.list.d/docker.list <<-EOF
+    deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(lsb_release -cs) stable
+EOF
+
+# Install packages
+apt-get --yes update
+apt-get --yes install qemu-guest-agent docker-ce
